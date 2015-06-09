9fbe5fde907087a0a6949fb68fcb473add1401cd
Martin Lehner <lehner.martinleo@bluewin.ch>
2015-06-09 11:13:14 +0200

    Added automatic saving of stopwatches. Replaced Microsoft new lines with unix new lines.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Stopwatch
-A simple web browser based stopwatch.
+A simple web browser based stopwatch. A live version is available at http://martinleolehner.github.io/stopwatch
+
+The watches are being stored in local storage.
--- stopwatch.js
+++ stopwatch.js
@@ -16,43 +16,140 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// Configuration
-var colorActive = "#97F578";  // Backgroundcolor for active stopwatches
-var colorInactive = "#ffffff";  // Backgroundcolor for inactive stopwatches
-
+// Variables and configuration
 var stopwatches = [];
-var concurrentWatches = false;  // Is it possible to run multiple watches at the same time?
 
-var intervalTime = 0.1;  // Update interval in seconds (0.1 recommended)
-var interval = null;
+var concurrentWatches = false;	// Is it possible to run multiple watches at the same time?
+var minuteFormat = false;	// true -> mmm:ss, false -> hh:mm:ss
+
+var updateTime = 0.1;	// Update interval in seconds (0.1 recommended)
+var updateInterval = null;
+
+var saveTime = 10;	// Interval time in seconds to save all states (10 recommended)
+var saveInterval = null;
 
 var useLocalStorage = true;
+
 var initialized = false;
 
-var minuteFormat = false;  // true -> mmm:ss, false -> hh:mm:ss
+var colorActive = "#97F578";	// Backgroundcolor for active stopwatches
+var colorInactive = "#ffffff";	// Backgroundcolor for inactive stopwatches
 
-window.onbeforeunload = function(){ return "If you leave this page, all states get lost."; }
 
-function init(){
+// Saves the watches before closing
+window.onbeforeunload = function()
+{
+	saveWatches();
+	//return "Do you really want to leave this page?";
+}
+
+// Initializes the stopwatch
+function init()
+{
 	useLocalStorage = localStorageAvailable();
+
 	if(getData("timeFormat") == "minute") minuteFormat = true;
 	document.getElementById("changeFormatCheckbox").checked = !minuteFormat;
-	updateTotalTime();
 
 	if(getData("concurrent") == "true") concurrentWatches = true;
 	document.getElementById("changeConcurrentCheckbox").checked = concurrentWatches;
 
 	initialized = true;
-	newStopwatch();
+
+	try
+	{
+		var watches = JSON.parse(getData("stopwatches"));
+		if(watches.length > 0)
+		{
+			for(var i=0; i<watches.length; i++)
+			{
+				newStopwatch(watches[i]);
+			}
+		}
+		else {
+			newStopwatch();
+		}
+	}
+	catch (e)
+	{
+		removeAll();
+	}
+
+	updateWatches();
+	updateTotalTime();
+
 }
 
+// Updates the total time
+function updateTotalTime()
+{
+	if(!initialized) return;
+
+	var totalTime = 0;
+	for(var i=0; i<stopwatches.length; i++)
+	{
+		if(stopwatches[i]["time"] != -1) totalTime += stopwatches[i]["time"];
+	}
+
+	if(totalTimeSpan = document.getElementById("totalTime"))
+	{
+		totalTime = Math.round(totalTime/1000);
+		totalTimeSpan.innerHTML = timeToFormattedString(totalTime);
+	}
+}
+
+// Updates all stopwatches
+function updateWatches()
+{
+	if (!initialized) return;
+
+	var now = new Date().getTime();
+	for(var i=0; i<stopwatches.length; i++)
+	{
+		var timeDiffrence = 0;
+		if(stopwatches[i]["active"] == true) timeDiffrence = now - stopwatches[i]["startTime"];
+		stopwatches[i]["time"] = stopwatches[i]["stoppedTime"] + timeDiffrence;
+		writeTime(stopwatches[i]["id"]);
+	}
+
+	updateTotalTime();
+}
+
+// Saves all stopwatches
+function saveWatches()
+{
+	if(!initialized) return;
+
+	var watches = [];
+	var now = new Date().getTime();
+
+	for(var i=0; i<stopwatches.length; i++)
+	{
+		var timeDiffrence = 0;
+		if(stopwatches[i]["active"] == true) timeDiffrence = now - stopwatches[i]["startTime"];
+		watches.push({
+			id: stopwatches[i]["id"],
+			active: false,
+			time: 0,
+			stoppedTime: stopwatches[i]["stoppedTime"] + timeDiffrence,
+			startTime: -1,
+			title: stopwatches[i]["title"],
+			description: stopwatches[i]["description"]});
+	}
+	setData("stopwatches", JSON.stringify(watches));
+}
+
+// Changes the setting to run multiple watches at the same time
 function changeConcurrent(val)
 {
+	if(!initialized) return;
+
 	if(val) concurrentWatches = true;
 	else
 	{
 		concurrentWatches = false;
 
+		// Get the number of active stopwatches
 		var activeWatches = 0;
 		for(var i=0; i<stopwatches.length; i++)
 		{
@@ -62,6 +159,7 @@ function changeConcurrent(val)
 			}
 		}
 
+		// Stop all watches except the last started if more than one watch is active
 		if(activeWatches > 1)
 		{
 			var latest = 0;
@@ -75,7 +173,7 @@ function changeConcurrent(val)
 
 			for(var i=0; i<stopwatches.length; i++)
 			{
-				if(stopwatches[i]["active"] && i != latest) stop(i);
+				if(stopwatches[i]["active"] && i != latest) stopWatch(i);
 			}
 		}
 	}
@@ -83,68 +181,23 @@ function changeConcurrent(val)
 	setData("concurrent", concurrentWatches);
 }
 
-function newStopwatch(){
-	if (!initialized) return;
-
-	stopwatches.push({active: false, time: 0, stoppedTime: 0, startTime: 0});
-	var watchNumber = stopwatches.length - 1;
-
-	var watchDiv = document.createElement("div");
-	watchDiv.className = "stopwatch";
-	watchDiv.id = "stopwatch" + watchNumber;
-	watchDiv.innerHTML = '<input type="text" class="title" placeholder="Title"><br>'
-	+ '<textarea class="description"  rows="2" cols="30" placeholder="Description"></textarea>'
-	+ '<p class="time">'+timeToFormattedString(0)+'</p>'
-	+ '<input type="button" class="start" value="Start" onclick="start('+watchNumber+');">'
-	+ '<input type="button" class="stop" value="Stop" onclick="stop('+watchNumber+');" disabled>'
-	+ '<input type="button" class="reset" value="Reset" onclick="reset('+watchNumber+');">'
-	+ '<input type="button" class="remove" value="Remove" onclick="removeWatch('+watchNumber+');">';
-
-	if(watchesDiv = document.getElementById("stopwatches")){
-		watchesDiv.appendChild(watchDiv);
-		watchDiv.getElementsByClassName("title")[0].focus();
-	}
-}
-
-function update(){
-	if (!initialized) return;
-
-	var now = new Date().getTime();
-	for(var i=0; i<stopwatches.length; i++){
-		if(stopwatches[i]["active"] == true){
-			var timeDiffrence = now - stopwatches[i]["startTime"];
-			stopwatches[i]["time"] = stopwatches[i]["stoppedTime"] + timeDiffrence;
-		}
-		writeTime(i);
-	}
-
-	updateTotalTime();
-}
-
-function updateTotalTime(){
-	var totalTime = 0;
-	for(var i=0; i<stopwatches.length; i++){
-		if(stopwatches[i]["time"] != -1) totalTime += stopwatches[i]["time"];
-	}
-
-	if(totalTimeSpan = document.getElementById("totalTime")){
-		totalTime = Math.round(totalTime/1000);
-		totalTimeSpan.innerHTML = timeToFormattedString(totalTime);
-	}
-}
-
+// Changes the format to minutes or hours
 function changeFormat(val)
 {
+	if(!initialized) return;
+
 	minuteFormat = !val;
 	setData("timeFormat", (minuteFormat)? "minute" : "hour");
-	update();
+	updateWatches();
 }
 
+// Formats raw seconds to a readable string
 function timeToFormattedString(totalSeconds)
 {
 	var seconds = totalSeconds%60;
 	var minutes = (totalSeconds - seconds)/60;
 	var timeString = "";
+
 	if(minuteFormat)
 	{
 		timeString = ((minutes<10)? "0" : "") + minutes + ":" + ((seconds<10)? "0" : "") + seconds;
@@ -155,38 +208,139 @@ function timeToFormattedString(totalSeconds)
 		minutes = minutes%60;
 		timeString = hours + ":" + ((minutes<10)? "0" : "") + minutes + ":" + ((seconds<10)? "0" : "") + seconds;
 	}
+
 	return timeString;
 }
 
-function writeTime(watchNumber){
-	if(stopwatchDiv = document.getElementById("stopwatch" + watchNumber)){
+// Creates a new stopwatch
+function newStopwatch(stopwatch)
+{
+	if (!initialized) return;
+
+	// Push an existing watch to the array or create a new one
+	if(typeof stopwatch == "object") stopwatches.push(stopwatch);
+	else
+	{
+		var newId = 0;
+		for(var i=0; i<stopwatches.length; i++)
+		{
+			if(stopwatches[i]["id"] >= newId) newId = stopwatches[i]["id"] + 1;
+		}
+
+		stopwatches.push({id: newId, active: false, time: 0, stoppedTime: 0, startTime: 0, title: "", description: ""});
+	}
+
+	var watchNumber = stopwatches.length - 1;
+	var watchId = stopwatches[watchNumber]["id"];
+
+	// Create the div
+	var watchDiv = document.createElement("div");
+	watchDiv.className = "stopwatch";
+	watchDiv.id = "stopwatch" + watchId;
+	watchDiv.innerHTML = '<input type="text" class="title" placeholder="Title" value="'+stopwatches[watchNumber]["title"]+'" onchange="saveTitle('+watchId+', this.value)">'
+	+ '<br>'
+	+ '<textarea class="description"  rows="2" cols="30" placeholder="Description" onchange="saveDescription('+watchId+', this.value)">'+stopwatches[watchNumber]["description"]+'</textarea>'
+	+ '<p class="time">'+timeToFormattedString(0)+'</p>'
+	+ '<input type="button" class="start" value="Start" onclick="startWatch('+watchId+');">'
+	+ '<input type="button" class="stop" value="Stop" onclick="stopWatch('+watchId+');" disabled>'
+	+ '<input type="button" class="reset" value="Reset" onclick="resetWatch('+watchId+');">'
+	+ '<input type="button" class="remove" value="Remove" onclick="removeWatch('+watchId+');">';
+
+	// Append the div
+	if(watchesDiv = document.getElementById("stopwatches"))
+	{
+		watchesDiv.appendChild(watchDiv);
+		if(typeof stopwatch != "object") watchDiv.getElementsByClassName("title")[0].focus();
+	}
+}
+
+// Gets the array index from the id
+function getWatchNumber(id)
+{
+	if(!initialized) return;
+
+	for(var i=0; i<stopwatches.length; i++)
+	{
+		if(id == stopwatches[i]["id"]) return i;
+	}
+
+	return -1;
+}
+
+// Saves the title
+function saveTitle(id, title)
+{
+	if(!initialized) return;
+
+	var watchNumber = getWatchNumber(id);
+
+	if(title != "") stopwatches[watchNumber]["title"] = title;
+}
+
+// Saves the description
+function saveDescription(id, description)
+{
+	if(!initialized) return;
+
+	var watchNumber = getWatchNumber(id);
+
+	if(description != "") stopwatches[watchNumber]["description"] = description;
+}
+
+// Writes the time into a stopwatch
+function writeTime(id)
+{
+	if(!initialized) return;
+
+	var watchNumber = getWatchNumber(id);
+
+	if(stopwatchDiv = document.getElementById("stopwatch" + id))
+	{
 		var totalSeconds = Math.round(stopwatches[watchNumber]["time"]/1000);
 
 		stopwatchDiv.getElementsByClassName("time")[0].innerHTML = timeToFormattedString(totalSeconds);
 	}
 }
 
-function start(watchNumber){
+// Starts a stopwatch
+function startWatch(id)
+{
 	if (!initialized) return;
 
 	if(!concurrentWatches) stopAll();
 
+	var watchNumber = getWatchNumber(id);
+
 	stopwatches[watchNumber]["active"] = true;
 	stopwatches[watchNumber]["startTime"] = new Date().getTime();
 
-	if(interval == null){
-		interval = setInterval(function(){update()},1000*intervalTime);
+	// Set the update inverval if not yet active
+	if(updateInterval == null)
+	{
+		updateInterval = setInterval(function(){ updateWatches() }, 1000*updateTime);
 	}
 
-	if(watchDiv = document.getElementById("stopwatch"+watchNumber)){
+	// Set the save interval if not yet active
+	if(saveInterval == null)
+	{
+		saveInterval = setInterval(function(){ saveWatches() }, 1000*saveTime);
+	}
+
+	// Update the watch's view
+	if(watchDiv = document.getElementById("stopwatch"+id))
+	{
 		watchDiv.getElementsByClassName("start")[0].disabled = true;
 		watchDiv.getElementsByClassName("stop")[0].disabled = false;
 		watchDiv.className = "stopwatch active";
 	}
 }
 
-function stop(watchNumber){
+// Stops a watch
+function stopWatch(id)
+{
 	if (!initialized) return;
+
+	var watchNumber = getWatchNumber(id);
 
 	if(!stopwatches[watchNumber]["active"]) return;
 
@@ -194,61 +348,102 @@ function stop(watchNumber){
 	stopwatches[watchNumber]["stoppedTime"] = stopwatches[watchNumber]["time"];
 
 	var allWatchesStopped = true;
-	var i = 0;
-	for(i=0; i<stopwatches.length; i++){
-		if(stopwatches[i]["active"] == true){
-			allWatchesStopped = false;
-		}
-	}
-	if(allWatchesStopped){
-		clearInterval(interval);
-		interval = null;
+	for(var i=0; i<stopwatches.length; i++)
+	{
+		if(stopwatches[i]["active"] == true) allWatchesStopped = false;
 	}
 
-	if(watchDiv = document.getElementById("stopwatch"+watchNumber)){
+	// Clear the update and save interval if all watches are stopped
+	if(allWatchesStopped)
+	{
+		clearInterval(updateInterval);
+		updateInterval = null;
+
+		clearInterval(saveInterval);
+		saveInterval = null;
+	}
+
+	// Update the watch's view
+	if(watchDiv = document.getElementById("stopwatch"+id))
+	{
 		watchDiv.getElementsByClassName("start")[0].disabled = false;
 		watchDiv.getElementsByClassName("stop")[0].disabled = true;
 		watchDiv.className = "stopwatch";
 	}
 }
 
-function stopAll(){
+// Resets a watch
+function resetWatch(id)
+{
 	if (!initialized) return;
 
-	for(var i=0; i<stopwatches.length; i++){
-		if(stopwatches[i]["active"] == true){
-			stop(i);
-		}
-	}
-}
+	stopWatch(id);
 
-function reset(watchNumber){
-	if (!initialized) return;
-
-	stop(watchNumber);
+	var watchNumber = getWatchNumber(id);
 
 	stopwatches[watchNumber]["time"] = 0;
 	stopwatches[watchNumber]["stoppedTime"] = 0;
 
-	writeTime(watchNumber);
+	writeTime(id);
 }
 
-function removeWatch(watchNumber){
+// Stops and removes a watch
+function removeWatch(id)
+{
 	if (!initialized) return;
 
-	stop(watchNumber);
+	stopWatch(id);
 
-	if(watchDiv = document.getElementById("stopwatch"+watchNumber)){
+	if(watchDiv = document.getElementById("stopwatch"+id))
+	{
 		watchDiv.parentElement.removeChild(watchDiv);
 	}
 
-	stopwatches[watchNumber]["time"] = -1;
+	var watchNumber = getWatchNumber(id);
+
+	stopwatches.splice(watchNumber, 1);
 
 	updateTotalTime();
 
 }
 
-// Check if local storage is available
+// Stops all watches
+function stopAll()
+{
+	if (!initialized) return;
+
+	for(var i=0; i<stopwatches.length; i++)
+	{
+		if(stopwatches[i]["active"] == true)
+		{
+			stopWatch(stopwatches[i]["id"]);
+		}
+	}
+}
+
+// Removes all stopwatches and adds one new stopwatch
+function removeAll()
+{
+	if (!initialized) return;
+
+	stopAll();
+
+	for(var i=0; i<stopwatches.length; i++)
+	{
+		if(watchDiv = document.getElementById("stopwatch"+stopwatches[i]["id"]))
+		{
+			watchDiv.parentElement.removeChild(watchDiv);
+		}
+	}
+
+	stopwatches = [];
+
+	newStopwatch();
+
+	updateTotalTime();
+}
+
+// Checks if local storage is available
 function localStorageAvailable()
 {
 	var x = "CheckForLocalStorage";
@@ -264,7 +459,7 @@ function localStorageAvailable()
 	}
 }
 
-// Sets a cookie or local storage
+// Sets a cookie or a local storage item
 function setData(name, value, days)
 {
 	if(useLocalStorage) localStorage.setItem(name, value);  // Set a local storage item
@@ -279,7 +474,7 @@ function setData(name, value, days)
 
 }
 
-// Gets a cookie or local storage
+// Gets a cookie or a local storage item
 function getData(name)
 {
 	if(useLocalStorage) return localStorage.getItem(name);  // Get the local storage item
